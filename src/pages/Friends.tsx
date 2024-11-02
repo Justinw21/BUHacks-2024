3ea664b9400930913f0c556b1f43baa5c07f4c76
Aaron Liu <99209428+AaronLiu615@users.noreply.github.com>
2024-11-02 14:48:02 -0400

    added nav bar to every pages

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -1,7 +1,10 @@
+import BottomNavbar from '@/components/ui/navbar';
+
 function Friends() {
     return (
         <div>
             <h2>Friends Page</h2>
+            <BottomNavbar />
         </div>
     );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,10 @@
+import BottomNavbar from '@/components/ui/navbar';
+
 function Homepage() {
     return (
         <div>
             <h2>Home Page</h2>
+            <BottomNavbar />
         </div>
     );
 }
