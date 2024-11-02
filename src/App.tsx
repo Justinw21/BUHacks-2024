2093e637d5ef5f8749dfe77800226f1b968d53a5
samantha <samanthap8675@gmail.com>
2024-11-02 14:55:30 -0400

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Homepage from './pages/Homepage';
 import History from './pages/History';
 import Friends from './pages/Friends';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/" element={<Homepage />} />
       <Route path="/history" element={<History />} /> 
       <Route path="/friends" element={<Friends />} /> 
+      <Route path="/signin" element={<SignIn/>} />
+      <Route path = "/signup" element={<SignUp/>} /> 
     </Routes>
   );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,20 @@
+import { logOut } from "@/services/authServices";
+import { useNavigate} from 'react-router-dom';
+
 function Homepage() {
+    const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    await logOut();
+    navigate('/signin');
+  };
     return (
         <div>
             <h2>Home Page</h2>
+
+            <button onClick={handleSignOut}>
+      Sign Out
+    </button>
         </div>
     );
 }
