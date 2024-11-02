8c5f6ff5589c852800530cf1df6b66c7b9aff7e8
samantha <samanthap8675@gmail.com>
2024-11-02 15:27:03 -0400

    bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes,  } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage';
 import History from './pages/History';
 import Friends from './pages/Friends';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+
 
 function App() {
   return (
